5150ea4d7493aa190bff669b37e7b6fb9c8f5be2
Katarina Nedovic <kacanedovic@gmail.com>
2025-08-04 19:57:48 +0200

    meni sajta

---
+++ ebiblioteka-ui/src/komponente/Meni.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {FaHouse} from "react-icons/fa6";
+import {FaBomb, FaBook, FaSign, FaSignInAlt, FaUser, FaWaveSquare} from "react-icons/fa";
+
+const Meni = () => {
+
+    const user = JSON.parse(window.sessionStorage.getItem('user'));
+    const token = window.sessionStorage.getItem('token');
+    const uloga = user ? user.uloga : null;
+    const imaPristupAdministraciji = uloga === 'admin' || uloga === 'bibliotekar';
+
+    return (
+        <>
+            <Navbar className="meni-bg" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="/">Ebiblioteka</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/"><FaHouse/> Pocetna</Nav.Link>
+                        <Nav.Link href="/knjige"><FaBook/> Knjige</Nav.Link>
+                        <Nav.Link href="/onama"><FaBomb/> O nama</Nav.Link>
+
+                        { token && (
+                            <>
+                            <Nav.Link href="/moje-knjige"><FaUser/> Moje knjige</Nav.Link>
+                                {
+                                    imaPristupAdministraciji && (
+                                        <Nav.Link href="/admin"><FaSign/> Admin</Nav.Link>
+                                    )
+                                }
+                            <Nav.Link href="/logout"><FaWaveSquare/> Logout</Nav.Link>
+                            </>
+                        )}
+
+                        { !token && (
+                            <>
+                                <Nav.Link href="/login"><FaSignInAlt/> Login</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Meni;
