16d772fa50b00889e3f75ca00a29dd78950ab52a
Katarina Nedovic <kacanedovic@gmail.com>
2025-08-04 20:12:26 +0200

    rutiranje stranica

--- ebiblioteka-ui/src/App.js
+++ ebiblioteka-ui/src/App.js
@@ -2,11 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Meni from "./komponente/Meni";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Pocetna from "./stranice/Pocetna";
+import Knjige from "./stranice/Knjige";
+import Onama from "./stranice/Onama";
+import MojeKnjige from "./stranice/MojeKnjige";
+import Admin from "./stranice/Admin";
+import Login from "./stranice/Login";
 
 function App() {
   return (
     <>
       <Meni />
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Pocetna />} />
+                <Route path="/knjige" element={<Knjige />} />
+                <Route path="/onama" element={<Onama />} />
+                <Route path="/moje-knjige" element={<MojeKnjige />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/login" element={<Login />} />
+
+            </Routes>
+        </BrowserRouter>
     </>
   );
 }
